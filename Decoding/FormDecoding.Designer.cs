91eec690309ae015ac32c032d4d559deedb37974
Kiril Spasov <spasov.kiril@gmail.com>
2022-11-30 12:45:48 +0200

    Add project files.

---
+++ Decoding/FormDecoding.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace Decoding
+{
+    partial class FormDecoding
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnDecode = new System.Windows.Forms.Button();
+            this.TxtResult = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // BtnDecode
+            // 
+            this.BtnDecode.Location = new System.Drawing.Point(38, 56);
+            this.BtnDecode.Name = "BtnDecode";
+            this.BtnDecode.Size = new System.Drawing.Size(193, 61);
+            this.BtnDecode.TabIndex = 0;
+            this.BtnDecode.Text = "Decode";
+            this.BtnDecode.UseVisualStyleBackColor = true;
+            this.BtnDecode.Click += new System.EventHandler(this.BtnDecode_Click);
+            // 
+            // TxtResult
+            // 
+            this.TxtResult.Location = new System.Drawing.Point(272, 56);
+            this.TxtResult.Multiline = true;
+            this.TxtResult.Name = "TxtResult";
+            this.TxtResult.Size = new System.Drawing.Size(446, 318);
+            this.TxtResult.TabIndex = 1;
+            // 
+            // FormDecoding
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.TxtResult);
+            this.Controls.Add(this.BtnDecode);
+            this.Name = "FormDecoding";
+            this.Text = "Decoding";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnDecode;
+        private System.Windows.Forms.TextBox TxtResult;
+    }
+}
+
---
+++ Decoding/FormDecoding.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Decoding
+{
+    public partial class FormDecoding : Form
+    {
+        public FormDecoding()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnDecode_Click(object sender, EventArgs e)
+        {
+            string line = "";
+            
+            string path = Application.StartupPath + @"/decode.txt";
+            StreamReader streamReader = new StreamReader(path);
+
+            int lineCount = Convert.ToInt32(streamReader.ReadLine());
+
+            for (int i = 1; i <= lineCount; i++)
+            {
+                line = streamReader.ReadLine();
+                TxtResult.Text += Decode(line) + Environment.NewLine;
+            }
+        }
+
+        private string Decode(string word)
+        {
+            string[] letters = new string[] { "a", "b", "d", "e", "h", "l", "o", "r", "w" };
+
+            string result = "";
+            string digit = "";
+            int zerosCount = 0;
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                digit = word.Substring(i, 1);
+
+                if (digit == "1")
+                {
+                    result += letters[zerosCount];
+                    zerosCount = 0;
+                }
+                else
+                {
+                    zerosCount++;
+                }
+            }
+            return result;
+        }
+    }
+}
